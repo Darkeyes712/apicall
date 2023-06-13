5499f98591af1bb9e8a52d3463a903ce4e036389
Nikola Kolev <nikolakolev712@gmail.com>
2023-06-13 18:52:02 +0300

    conversion to hashmap works, but still has work to do

--- src/lib.rs
+++ src/lib.rs
@@ -19,35 +19,57 @@ pub fn get_user_input(user_input:&str) -> String{
     return usr_inp;
 }
 
+
+// #[tokio::main]
+// pub async fn create_get_request() -> Result<(), Box<dyn std::error::Error>> {
+//     let response = reqwest::get("http://192.168.1.242:80/api/users").await?.text().await?;
+
+//     if let Ok(json_response) = serde_json::from_str::<serde_json::Value>(&response) {
+//         if let serde_json::Value::Object(obj) = json_response {
+//             let hashmap: std::collections::HashMap<String, String> = obj
+//                 .iter()
+//                 .filter_map(|(k, v)| {
+//                     if let serde_json::Value::String(s) = v {
+//                         Some((k.clone(), s.clone()))
+//                     } else {
+//                         Some((k.clone(), v.to_string()))
+//                     }
+//                 })
+//                 .collect();
+            
+//             // Print the hashmap
+//             for (key, value) in &hashmap {
+//                 println!("Key: {}, Value: {}", key, value);
+//             }
+//         } else {
+//             println!("The JSON response is not an object.");
+//         }
+//     } else {
+//         println!("Failed to parse JSON response.");
+
+//         // Process the response as a string
+//         let hashmap: std::collections::HashMap<String, String> = vec![("response".to_owned(), response)].into_iter().collect();
+        
+//         // Print the hashmap
+//         for (key, value) in &hashmap {
+//             println!("Key: {}, Value: {}", key, value);
+//         }
+//     }
+
+//     Ok(())
+// }
+
+
 #[tokio::main]
 pub async fn create_get_request() -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get("http://192.168.1.242:80/api/users").await?.text().await?;
-    let json_response: serde_json::Value = serde_json::from_str(&response)?;
-    
-    println!("{}", json_response);
 
-    if let Some(obj) = json_response.as_object() {
-        let hashmap: std::collections::HashMap<String, String> = obj
-            .iter()
-            .filter_map(|(k, v)| {
-                if let serde_json::Value::String(s) = v {
-                    Some((k.clone(), s.clone()))
-                } else {
-                    None
-                }
-            })
-            .collect();
-        // NOTE: Figure out how to make the hashmap work so that it can be printed. 
-        // for (key, value) in hashmap {
-        //     println!("Key: {}, Value: {}", key, value);
-        // }
-        println!("{:?}", hashmap);
+    let hashmap: std::collections::HashMap<String, String> = vec![("response".to_owned(), response)].into_iter().collect();
+        
+    // Print the hashmap
+    for (key, value) in &hashmap {
+        println!("Key: {}, Value: {}", key, value);
     }
 
     Ok(())
 }
-
-// pub fn create_post_request() {
-//     println!("POST");
-// }
-
